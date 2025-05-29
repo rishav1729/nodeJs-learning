d9dded31d06f9b35574099e68df50a76111f1a21
rishav1729 <1729rishavranjan@gmail.com>
2025-05-29 23:59:11 +0530

    model changes

--- devtinder/src/app.js
+++ devtinder/src/app.js
@@ -2,9 +2,42 @@
 // here we will initialse our app
 const express = require("express")
 const connectDB = require("./config/database")
-
+const User = require("./models/user")   
 const app = express() //instance of express js application, this line create a web server
                       //when we create a webserver, we have to call listen over there and we have to listen on some port, so that anybody can connect to us
+
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+    res.send("users");
+})
+
+app.get("/users/:id", (req, res) => {
+    res.send(`user ${req.params.id}`);
+})
+
+app.delete("/users/:id", (req, res) => {
+    res.send(`user ${req.params.id} deleted`);
+})
+
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    const updateData = req.body;
+    try {
+        const allowedUpdates = [ "profilePicture", "bio"];
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.send(user);
+    } catch(err) {
+        res.status(500).send("Error updating user: " + err.message);
+    }
+})
+
+app.post("/signup", (req, res) => {
+
+    console.log(req.body);
+    res.send("user created");
+});
+
 
 connectDB().then(() => {
     console.log("successfully connected to db"); 
--- devtinder/src/models/user.js
+++ devtinder/src/models/user.js
@@ -1,27 +1,72 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30,
+        trim: true,
+        lowercase: true
     },
     lastName: {
         type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30,
+        trim: true,
+        lowercase: true
     },
     email: {
         type: String,
         unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid email address: " + value);
+            }
+        }
     },
     password: {
         type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 1024,
+        validate: {
+            validator: validator.isStrongPassword,
+            message: "Password must be strong"
+        }
     },
     age: {
         type: Number,
+        min: 18,
+        max: 100
     },  
     gender: {
         type: String,
+        enum: ["male", "female", "other"],
+        validate(value) {
+            if (!validator.isIn(value, ["male", "female", "other"])) {
+                throw new Error("Invalid gender: " + value);
+            }
+        }
     },
-                   
-})
+    profilePicture: {
+        type: String,
+        default: "default-profile-picture.jpg",
+        validate: {
+            validator: validator.isURL,
+            message: "Invalid profile picture URL"
+        }
+    },
+    bio: {
+        type: String,
+        maxlength: 200
+    },
+})  
 
 const User = mongoose.model("User", userSchema);
 
